693182e66fe1f3da28fedad5fb09aa0577bd7c82
louisaaronmiller <lmiller12@qub.ac.uk>
2025-12-13 16:24:40 +0000

    Update BDMC.py

--- BDMC.py
+++ BDMC.py
@@ -19,6 +19,12 @@ q,q1 are seeded from the uniform interval (0,q0)  ------ This affects the accept
 chi is seeded from the unfirom interval (-1,1)    ------ This affects the acceptance ratios (simplifying them)
 '''
 
+# ================================ CONSTANTS ================================
+
+epsilon = 1e-5
+
+# ================================ FUNCTIONS ================================
+
 def heaviside(q0,q):
     if q0 < q:
         return 0
@@ -42,7 +48,7 @@ def lambda_seed(lam,q):
 def u_0(potential,r_star,mass):
     return (2/3) * mass * potential * (r_star ** 3)
 
-def u(q,potential,r_star,mass,eps = 1e-5):
+def u(q,potential,r_star,mass,eps = epsilon):
     if q <= eps:
         return u_0(potential,r_star,mass) * (1 - (q**2)/(2))
     return (3 * u_0(potential,r_star,mass)) * (np.sin(q) - q * np.cos(q))/(q ** 3)
@@ -65,100 +71,104 @@ def O01(q,q0):
     omega = heaviside(q0,q)/q0
     return omega
 
-def R01(q,q0,A01,D0):
-    acceptance_ratio = (A01 * q0 * abs(u(q))) / (D0)
+def R01(q,q0,A01,D0,potential,r_star,mass,eps = epsilon):
+    acceptance_ratio = (A01 * q0 * abs(u(q,potential,r_star,mass,eps = eps))) / (D0)
     return acceptance_ratio
 
-def Type0to1(q,q0,D0,A01=1):
+def Type0to1(r,q,q0,D0,potential,r_star,mass,eps = epsilon,A01=1):
     '''
     Returns True if you accept type swap
     Returns False if you reject type swap
 
     Addressal probability is equal to one. (A01= 1)
     '''
-    if np.random.random() <= Pxy(R01(q,q0,A01,D0)):
+    assert 0 <= r <= 1
+    if r <= Pxy(R01(q,q0,A01,D0,potential,r_star,mass,eps = eps)):
         return True
     else:
         return False
 
 # ================================ TYPE 1 ---> 0 ================================
 
-def R10(q,q0,A10,D0):
-    acceptance_ratio = ((D0) / (A10 * abs(u(q)))) * heaviside(q0,q)
+def R10(q,q0,A10,D0,potential,r_star,mass,eps = epsilon):
+    acceptance_ratio = ((D0) / (A10 * abs(u(q,potential,r_star,mass,eps = eps)))) * heaviside(q0,q)
     return acceptance_ratio
 
-def Type1to0(A10,q0,q,D0):
-    if np.random.random() <= A10:
-        if np.random.random() <= Pxy(R10(q,q0,A10,D0)):
+def Type1to0(r,A10,q0,q,D0,potential,r_star,mass,eps = epsilon):
+    assert 0 <= r <= 1
+    if r <= A10:
+        if np.random.random() <= Pxy(R10(q,q0,A10,D0,potential,r_star,mass,eps = eps)):
             return True
     return False
 
 
 # ================================ TYPE 1 ---> 2 ================================
 
-def O12(q0,q1): # notes say dependence on q and chi are here, but they're not?
-    heaviside(q0,q1) / (2 * q0)
-
-def R12(A12,A21,q0,q1,q,chi,deltaq,H):
-    acceptance_ratio = ( (A21) / (A12) ) * ( (2*q0*abs(u(np.sqrt((q**2) + (q1**2) - 2*q*q1*chi)) * H[get_bin(q1,deltaq)])) / (np.pi * abs(u(q))) )
+def R12(A12,A21,q0,q1,q,chi,deltaq,H,potential,r_star,mass,eps = epsilon):
+    acceptance_ratio = ( (A21) / (A12) ) * ( (2*q0*abs(u(np.sqrt((q**2) + (q1**2) - 2*q*q1*chi)) * H[get_bin(q1,deltaq)])) / (np.pi * abs(u(q,potential,r_star,mass,eps = eps))) )
     return acceptance_ratio
 
-def Type1to2(A12,A21,q0,q1,q,chi):
+def Type1to2(r,A12,A21,q0,q1,q,chi,deltaq,H,potential,r_star,mass,eps = epsilon):
     '''
     q is taken from type 1 diagram
     q1 and chi are seeded from (0,q0) and (-1,1) respectively.
     '''
-    if np.random.random() <= A12:
-        if np.random.random() <= Pxy(R12(A12,A21,q0,q1,q,chi)):
+    assert 0 <= r <= 1
+    if r <= A12:
+        if np.random.random() <= Pxy(R12(A12,A21,q0,q1,q,chi,deltaq,H,potential,r_star,mass,eps = eps)):
             return True
     return False
 
 # ================================ TYPE 1 ---> VARIABLE ================================
 
-def R1(q_prime,q):
-    acceptance_ratio = (abs(u(q_prime)) * q) / (abs(u(q)) * q_prime)
+def R1(q_prime,q,potential,r_star,mass,eps = epsilon):
+    acceptance_ratio = (abs(u(q_prime,potential,r_star,mass,eps = eps)) * q) / (abs(u(q,potential,r_star,mass,eps = eps)) * q_prime)
     return acceptance_ratio
 
-def variables1(A10,A12,lam,q):
-    if np.random.random() <= (1 - A10 - A12):
+def variables1(r,A10,A12,lam,q,potential,r_star,mass,eps = epsilon):
+    assert 0 <= r <= 1
+    if r <= (1 - A10 - A12):
         q_prime = lambda_seed(lam,q)
-        if np.random.random <= Pxy(R1(q_prime,q)):
-            return True,q_prime
-    return False
+        if np.random.random() <= Pxy(R1(q_prime,q,potential,r_star,mass,eps = eps)):
+            return q_prime # True
+    return q               # False
 
 # ================================ TYPE 2 ---> 1 ================================
 
-def R21(A12,A21,q0,q1,q,chi,deltaq,H):
-    acceptance_ratio = heaviside(q0,q1) * ( (A12) / (A21) ) * ( (np.pi * abs(u(q))) / (2*q0*abs(u(np.sqrt((q**2) + (q1**2) - 2*q*q1*chi)) * H[get_bin(q1,deltaq)])) )
+def R21(A12,A21,q0,q1,q,chi,deltaq,H,potential,r_star,mass,eps = epsilon):
+    acceptance_ratio = heaviside(q0,q1) * ( (A12) / (A21) ) * ( (np.pi * abs(u(q,potential,r_star,mass,eps = eps))) / (2*q0*abs(u(np.sqrt((q**2) + (q1**2) - 2*q*q1*chi),potential,r_star,mass,eps = eps) * H[get_bin(q1,deltaq)])) )
     return acceptance_ratio
 
-def Type2to1(A12,A21,q0,q1,q,chi):
+def Type2to1(r,A12,A21,q0,q1,q,chi,deltaq,H,potential,r_star,mass,eps = epsilon):
     '''
     q is taken from type 2 diagram
     q1 and chi are seeded from (0,q0) and (-1,1) respectively.
     '''
-    if np.random.random() <= A21:
-        if np.random.random() <= Pxy(R21(A12,A21,q0,q1,q,chi)):
+    assert 0 <= r <= 1
+    if r <= A21:
+        if np.random.random() <= Pxy(R21(A12,A21,q0,q1,q,chi,deltaq,H,potential,r_star,mass,eps = eps)):
             return True
     return False
 
 # ================================ TYPE 2 ---> VARIABLE ================================
 
-def R2(q,q1,qp,qp1,chi,deltaq,H):
+def R2(q,q1,qp,qp1,chi,deltaq,H,potential,r_star,mass,eps = epsilon):
     '''
     qp = q prime
     qp1 = q prime 1
     '''
-    acceptance_ratio = (abs(u(np.sqrt(qp**2 + qp1**2 - 2*qp*qp1*chi)) * H[get_bin(qp1,deltaq)]) * q*q1) / (abs(u(np.sqrt(q**2 + q1**2 - 2*q*q1*chi)) * H[get_bin(q1,deltaq)]) * qp*qp1)
+    acceptance_ratio = (abs(u(np.sqrt(qp**2 + qp1**2 - 2*qp*qp1*chi),potential,r_star,mass,eps = eps) * H[get_bin(qp1,deltaq)]) * q*q1) / (abs(u(np.sqrt(q**2 + q1**2 - 2*q*q1*chi),potential,r_star,mass,eps = eps) * H[get_bin(q1,deltaq)]) * qp*qp1)
     return acceptance_ratio
 
-def variables2(A21,lam,q,q1,chi):
-    if np.random.random() <= (1 - A21):
+def variables2(r,q0,A21,lam,q,q1,chi,deltaq,H,potential,r_star,mass,eps = epsilon):
+    assert 0 <= r <= 1
+    if r <= (1 - A21):
+        chi = np.random.uniform(-1, 1)
         qp = lambda_seed(lam,q)
         qp1 = lambda_seed(lam,q1)
-        if np.random.random <= Pxy(R2(q,q1,qp,qp1,chi)):
-            return True,qp,qp1
-    return False
+        if np.random.random() <= Pxy(R2(q,q1,qp,qp1,chi,deltaq,H,potential,r_star,mass,eps = eps)):
+            return qp,qp1,chi # True
+    return q,q1,chi           # False
 
 # ================================ UPDATE FOR TYPE 0 ================================
 '''
@@ -181,12 +191,12 @@ def U(type:int,q,H,deltaq):
 
 # ================================ ALGORITHM ================================
 
-def DiagMC(N,A10,A12,A21,q0,D0,deltaq,lam):
-
+def DiagMC(N,A10,A12,A21,q0,D0,deltaq,lam,potential,r_star,mass,eps = epsilon):
+    # assert A12 + A21 == 1 
     q,q1,chi = seed(q0)
 
     Nbins = int(q0 / deltaq)
-    H = np.ones(Nbins) * 1e-6   
+    H = np.zeros(Nbins) * 1e-6   
     Type = 1 # Starting in type 1
 
     type0sum = 0
@@ -194,11 +204,11 @@ def DiagMC(N,A10,A12,A21,q0,D0,deltaq,lam):
     type2sum = 0
 
     for _ in range(N):
-
+        r = np.random.random()
 
 
         if Type == 0:
-            if Type0to1(q,q0,D0,A01=1):
+            if Type0to1(r,q,q0,D0,potential,r_star,mass,eps = eps,A01=1):
                 Type = 1
                 type0sum += 1
                 continue
@@ -209,18 +219,18 @@ def DiagMC(N,A10,A12,A21,q0,D0,deltaq,lam):
 
 
         if Type == 1:
-            if Type1to0(A10,q0,q,D0):
+            if Type1to0(r,A10,q0,q,D0,potential,r_star,mass,eps = eps):
                 Type = 0
                 type1sum += 1
                 continue
 
-            elif Type1to2(A12,A21,q0,q1,q,chi):
+            elif Type1to2(r,A12,A21,q0,q1,q,chi,deltaq,H,potential,r_star,mass,eps = eps):
                 Type = 2
                 type1sum += 1
                 continue
 
             else:
-                _,q = variables1(A12,A21,q0,q1,q,chi)
+                q = variables1(r,A10,A12,lam,q,potential,r_star,mass,eps = eps)
                 H = U(1,q,H,deltaq)
                 type1sum += 1
                 continue
@@ -228,13 +238,13 @@ def DiagMC(N,A10,A12,A21,q0,D0,deltaq,lam):
 
 
         if Type == 2:
-            if Type2to1(A12,A21,q0,q1,q,chi):
+            if Type2to1(r,A12,A21,q0,q1,q,chi,deltaq,H,potential,r_star,mass,eps = eps):
                 Type = 1
                 type2sum += 1
                 continue
 
             else:
-                _,q,q1 = variables2(A21,lam,q,q1,chi)
+                q,q1,chi = variables2(r,q0,A21,lam,q,q1,chi,deltaq,H,potential,r_star,mass,eps = eps)
                 H = U(2,q,H,deltaq)
                 type2sum += 1
                 continue
@@ -246,11 +256,4 @@ def DiagMC(N,A10,A12,A21,q0,D0,deltaq,lam):
 
 
 
-# fix all calls to u(q) to suit needs to new u() that includes r_star
-# whatever i need to do to fix r_star, and find out what to do about it
-# fix R12,R21, R2 such that they all recieve H,delta in there function signatures.
-# in type1, add a random number instead of calling it, since there are two moves that take place, a single random number is better
-# same for type2
-# just like i update q,q1, i need to update chi from type2
-
-
+# When this seeds values i.e. q,q1; it does it with np.random.uniform()
